069d9892a869f9d0e42dd283adcdfe98f874d77e
Nuno Maduro <enunomaduro@gmail.com>
2023-11-05 09:20:27 -0500

    chore: uses `example.com` instead

--- tests/Expectations/Requests.php
+++ tests/Expectations/Requests.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 use function Pest\Stressless\stress;
 
 it('may pass', function (): void {
-    $result = stress('nunomaduro.com')
+    $result = stress('example.com')
         ->with(2)->concurrentRequests()
         ->for(1)->second();
 
@@ -13,7 +13,7 @@ it('may pass', function (): void {
 });
 
 it('may fail', function (): void {
-    $result = stress('dummy-nunomaduro.com')
+    $result = stress('dummy-example.com')
         ->with(2)->concurrentRequests()
         ->for(1)->second();
 
