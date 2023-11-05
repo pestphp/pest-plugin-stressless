b132a7867c970b63dde3e35d013f6afdd97e07a6
Nuno Maduro <enunomaduro@gmail.com>
2023-11-05 09:09:21 -0500

    chore: fixes style

--- tests/Unit/Binary.php
+++ tests/Unit/Binary.php
@@ -9,7 +9,7 @@ it('infers the path from the environment on mac OS', function (): void {
 
     $arch = str_contains(php_uname('m'), 'arm') ? 'arm64' : 'amd64';
 
-    expect((string) $binary)->toBe(realpath(__DIR__.'/../../bin/k6-macos-'. $arch));
+    expect((string) $binary)->toBe(realpath(__DIR__.'/../../bin/k6-macos-'.$arch));
 })->skipOnLinux()->skipOnWindows();
 
 it('infers the path from the environment on Linux', function (): void {
@@ -17,5 +17,5 @@ it('infers the path from the environment on Linux', function (): void {
 
     $arch = str_contains(php_uname('m'), 'arm') ? 'arm64' : 'amd64';
 
-    expect((string) $binary)->toBe(realpath(__DIR__.'/../../bin/k6-linux-'. $arch));
+    expect((string) $binary)->toBe(realpath(__DIR__.'/../../bin/k6-linux-'.$arch));
 })->skipOnMac()->skipOnWindows();
