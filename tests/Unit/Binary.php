46034394fa02b1ccbfd7ce10377fc5f70c9e5c91
Nuno Maduro <enunomaduro@gmail.com>
2023-11-05 09:06:21 -0500

    chore: adjusts K6 test

--- tests/Unit/Binary.php
+++ tests/Unit/Binary.php
@@ -4,8 +4,18 @@ declare(strict_types=1);
 
 use Pest\Stressless\ValueObjects\Binary;
 
-it('infers the path from the environment', function (): void {
+it('infers the path from the environment on mac OS', function (): void {
     $binary = Binary::k6();
 
-    expect((string) $binary)->toBe(realpath(__DIR__.'/../../bin/k6-macos-arm64'));
-});
+    $arch = str_contains(php_uname('m'), 'arm') ? 'arm64' : 'amd64';
+
+    expect((string) $binary)->toBe(realpath(__DIR__.'/../../bin/k6-macos-'. $arch));
+})->skipOnLinux()->skipOnWindows();
+
+it('infers the path from the environment on Linux', function (): void {
+    $binary = Binary::k6();
+
+    $arch = str_contains(php_uname('m'), 'arm') ? 'arm64' : 'amd64';
+
+    expect((string) $binary)->toBe(realpath(__DIR__.'/../../bin/k6-linux-'. $arch));
+})->skipOnMac()->skipOnWindows();
