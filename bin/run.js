9d0cbb55ca95ba6ef48951fe1ea488497635f6d4
Nuno Maduro <enunomaduro@gmail.com>
2023-11-06 12:08:39 +0000

    feat: user agent

--- bin/run.js
+++ bin/run.js
@@ -3,7 +3,9 @@ import http from 'k6/http';
 export const options = JSON.parse(__ENV.PEST_STRESS_TEST_OPTIONS);
 
 export default () => {
-    http.get(__ENV.PEST_STRESS_TEST_URL);
+    http.get(__ENV.PEST_STRESS_TEST_URL, {
+        headers: { 'user-agent': 'Pest Plugin Stressless (https://pestphp.com) + K6 (https://k6.io)' },
+    });
 }
 
 export function handleSummary(data) {
