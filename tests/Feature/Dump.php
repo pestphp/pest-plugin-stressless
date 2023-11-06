800fc7fe068130d0e8ef028c02c9224485d77c4d
Nuno Maduro <enunomaduro@gmail.com>
2023-11-06 10:48:46 +0000

    chore: fixes slow test suite

--- tests/Feature/Dump.php
+++ tests/Feature/Dump.php
@@ -5,8 +5,7 @@ declare(strict_types=1);
 use function Pest\Stressless\stress;
 
 it('can dump the result', function (): void {
-    $result = stress('example.com')
-        ->dump();
+    $result = $this->stress->dump();
 
     expect($result->requests()->duration()->avg)->toBeGreaterThan(0);
 });
